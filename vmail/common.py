4350c489540215e33a61bb1ca3f8bca4910b060c
Damien Churchill <damien.churchill@ukplc.net>
2010-06-11 17:42:03 +0100

    add fsize to common.py for formatting byte values

--- vmail/common.py
+++ vmail/common.py
@@ -252,3 +252,27 @@ def maildrop(msg, deliver_to, sender):
         raise VmailError('Unable to deliver message')
 
     return True
+
+def fsize(fsize_b):
+    """
+    Formats the bytes value into a string with KiB, MiB or GiB units
+
+    :param fsize_b: the filesize in bytes
+    :type fsize_b: int
+    :returns: formatted string in KiB, MiB or GiB units
+    :rtype: string
+
+    **Usage**
+
+    >>> fsize(112245)
+    '109.6 KiB'
+
+    """
+    fsize_kb = fsize_b / 1024.0
+    if fsize_kb < 1024:
+        return "%.1f KiB" % fsize_kb
+    fsize_mb = fsize_kb / 1024.0
+    if fsize_mb < 1024:
+        return "%.1f MiB" % fsize_mb
+    fsize_gb = fsize_mb / 1024.0
+    return "%.1f GiB" % fsize_gb
