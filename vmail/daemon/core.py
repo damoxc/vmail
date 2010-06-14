3aaba78581af5ea0e0af7fd5bb3cc9301cc7ea30
Damien Churchill <damien.churchill@ukplc.net>
2010-06-14 12:55:58 +0100

    add is_validrcptto and log_message methods to the core

--- vmail/daemon/core.py
+++ vmail/daemon/core.py
@@ -51,7 +51,70 @@ class Core(object):
             return db.query(Domain).filter_by(domain=domain).one().quota
 
     @export
+    def is_validrcptto(self, email):
+        """
+        Check whether an email address is a valid recipient.
+
+        :param email: The email address to check.
+        :type email: string
+        :returns: An integer value representing the result
+        :rtype: int
+        """
+        try:
+            result = db.execute('CALL is_validrcptto(:email)',
+                {'email': email})
+            row = result.fetchone()
+            result.close()
+        except Exception, e:
+            log.error('error checking database')
+            log.exception(e)
+            return 255
+
+        if not row:
+            log.critical('is_validrcptto() call failed with no result')
+            return 1
+
+        # see if we want to do some quota checking now
+        if row[2] == 'local' or (row[2] == 'forward' and row[3]):
+            if row[2] == 'forward':
+                email = row[1]
+
+            result = db.execute('CALL get_quotas(:email)', {'email': email})
+            row = result.fetchone()
+            if not row:
+                # we'd rather let a message through than fail on quota
+                return 0
+
+            try:
+                user_quota, domain_quota = row
+                (user, domain) = email.split('@')
+
+                if get_usage(domain, user) >= user_quota:
+                    # user is over quota, stop it before deliver
+                    return 4
+
+                if get_usage(domain) >= domain_quota:
+                    # domain is over quota
+                    return 5
+
+            except Exception, e:
+                log.error('error checking email %s', email)
+                log.exception(e)
+
+        return 0
+
+    @export
     def last_login(self, email, method, remote_addr=None):
+        """
+        Log a login made to the email system.
+
+        :param email: The email address used to log in
+        :type email: string
+        :param method: The method used to log in.
+        :type method: string
+        :keyword remote_addr: The remote address of the log in
+        :type remote_addr: string
+        """
         try:
             user = db.query(User).filter_by(email=email).one()
         except:
@@ -68,3 +131,38 @@ class Core(object):
         rw_db.add(login)
         rw_db.commit()
         return True
+
+    @export
+    def log_message(self, sender, user, subject, remote_addr=None,
+            recipients=()):
+        """
+        Log a message passing through the email system.
+
+        :param sender: The person sending the message
+        :type sender: string
+        :param user: The user sending the message
+        :type user: string
+        :param subject: The message subject
+        :type subject: string
+        :param remote_addr: The remote address of the message
+        :type remote_addr: string
+        :param recipients: The recipients of the message
+        :type recipients: sequence of strings
+        """
+
+        message = Message()
+        message.date = datetime.datetime.now()
+        message.sender = sender
+        message.user = user
+        message.subject = subject
+        message.local_addr = socket.getfqdn()
+        message.remote_addr = remote_addr
+        rw_db.add(message)
+        rw_db.commit()
+
+        for rcpt in recipients:
+            recipient = MessageRecipient()
+            recipient.message_id = message.id
+            recipient.recipient = rcpt
+            rw_db.add(recipient)
+        rw_db.commit()
