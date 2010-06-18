7e1d696386dd03af573067c289af3bdb1571c6e8
Damien Churchill <damien.churchill@ukplc.net>
2010-06-18 07:48:32 +0100

    change version to 0.3.4

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'vmail',
-    version      = '0.3.2',
+    version      = '0.3.3',
     author       = 'Damien Churchill',
     author_email = 'damoxc@gmail.com',
 
