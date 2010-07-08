a4a54d29b235b6adad101c8ea97fbc5dc9112856
Damien Churchill <damien.churchill@ukplc.net>
2010-07-08 11:02:41 +0100

    don't have the cronjob enabled by default

--- debian/vmail-smtp.cron.d
+++ debian/vmail-smtp.cron.d
@@ -4,5 +4,5 @@ HOME=/var/spool/qpsmtpd
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-*/10 * * * *   root     /usr/bin/sa-learn --ham /home/vmail/filter.example.com/ham/cur
-*/10 * * * *   root     /usr/bin/sa-learn --spam /home/vmail/filter.example/spam/cur
+#*/10 * * * *   root     /usr/bin/sa-learn --ham /home/vmail/filter.example.com/ham/cur
+#*/10 * * * *   root     /usr/bin/sa-learn --spam /home/vmail/filter.example/spam/cur
