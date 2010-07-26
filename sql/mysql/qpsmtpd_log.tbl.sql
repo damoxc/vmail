b4a90e544027d42888cabb170aee0c0792703dce
Damien Churchill <damien.churchill@ukplc.net>
2010-07-26 12:50:49 +0100

    add new table to store qpsmtpd log messages

---
+++ sql/mysql/qpsmtpd_log.tbl.sql
@@ -0,0 +1,36 @@
+--
+-- sql/mysql/qpsmtpd_log.tbl.sql
+--
+-- Copyright (C) 2010 @UK Plc, http://www.uk-plc.net
+--
+-- Author:
+--    2010 Damien Churchill <damoxc@gmail.com>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3, or (at your option)
+-- any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.    If not, write to:
+--   The Free Software Foundation, Inc.,
+--   51 Franklin Street, Fifth Floor
+--   Boston, MA    02110-1301, USA.
+--
+
+CREATE TABLE IF NOT EXISTS `qpsmtpd_log` (
+	`id`             int(11)      NOT NULL auto_increment,
+	`transaction_id` varchar(40)  NOT NULL,
+	`host`           varchar(80)  NOT NULL,
+	`hook`           varchar(20)  NOT NULL,
+	`plugin`         varchar(40)  NOT NULL,
+	`level`          int(1)       NOT NULL,
+	`message`        varchar(255) NOT NULL,
+	`date`           datetime     NOT NULL,
+	PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
