2cfe9c12ccfadea52f492db49c05b155c5d3fcca
Damien Churchill <damien.churchill@ukplc.net>
2010-09-23 11:09:30 +0100

    remove the import of all of model from test_model

--- vmail/tests/test.py
+++ vmail/tests/test.py
@@ -1,4 +1,5 @@
 from twisted.trial import unittest
+from vmail.model.tables import *
 
 class BaseUnitTest(unittest.TestCase):
     pass
@@ -11,12 +12,15 @@ class DatabaseUnitTest(BaseUnitTest):
 
     def setUp(self):
         from vmail.model import _create_engine, init_model, init_rw_model
-        from vmail.model.tables import *
         engine = _create_engine('sqlite:///')
         meta.create_all(bind=engine)
 
         init_model(engine)
         init_rw_model(engine)
+
+        from vmail.model import db, rw_db
+        self.db = db
+        self.rw_db = rw_db
 
 class CoreUnitTest(DatabaseUnitTest):
     """
--- vmail/tests/test_model.py
+++ vmail/tests/test_model.py
@@ -1,8 +1,8 @@
 from vmail.tests import test
-from vmail.model import *
+from vmail.model.classes import *
 
 class TestDatabase(test.DatabaseUnitTest):
 
     def test_domains(self):
-        domain = db.query(Domain).get(1)
+        domain = self.db.query(Domain).get(1)
         self.assertTrue(isinstance(domain, Domain))
