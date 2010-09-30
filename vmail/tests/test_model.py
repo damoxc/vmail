66daaf8e657aa4b87221e1e3db7d3fcf94dc539e
Damien Churchill <damien.churchill@ukplc.net>
2010-09-30 11:49:23 +0100

    add more tests demonstrating getting a user, adding a user and removing a user to the model

--- vmail/tests/test_model.py
+++ vmail/tests/test_model.py
@@ -6,3 +6,35 @@ class TestDatabase(test.DatabaseUnitTest):
     def test_domains(self):
         domain = self.db.query(Domain).get(1)
         self.assertTrue(isinstance(domain, Domain))
+
+    def test_users(self):
+        user = self.db.query(User).get(1)
+        self.assertTrue(isinstance(user, User))
+
+    def test_adding_user(self):
+        usage = UserQuota()
+        usage.bytes = 123123
+        usage.messages = 83
+
+        user_count = self.db.query(User).count()
+
+        user = User()
+        user.domain_id = 1
+        user.email = 'joe.bloggs@example.com'
+        user.name = 'Joe Bloggs'
+        user.password = 'somesecret'
+        user.quota = 52428800
+        user.usage = usage
+        self.db.add(user)
+        self.db.commit()
+
+        _user = self.db.query(User
+            ).filter_by(email='joe.bloggs@example.com').one()
+        self.assertTrue(user_count < self.db.query(User).count())
+        self.assertEqual(_user.name, 'Joe Bloggs')
+        self.assertEqual(_user.usage.bytes, 123123)
+
+    def test_removing_user(self):
+        user_count = self.db.query(User).count()
+        self.db.query(User).filter_by(id=4).delete()
+        self.assertTrue(user_count > self.db.query(User).count())
