468740b92f8598c56b8a83efa6907138cdc25633
Damien Churchill <damien.churchill@ukplc.net>
2010-10-20 15:14:50 +0100

    convert vchkpasswd over to use the new base class

--- vmail/scripts/vchkpasswd.py
+++ vmail/scripts/vchkpasswd.py
@@ -24,33 +24,31 @@
 #
 
 from vmail.client import client, reactor
-from vmail.scripts.base import ScriptBase, argcount
+from vmail.scripts.base import DaemonScriptBase, argcount
 
-class VChkPasswd(ScriptBase):
+class VChkPasswd(DaemonScriptBase):
 
     script = 'vchkpasswd'
     usage  = 'Usage: %prog [options] user passwd'
 
     def on_connect(self, result):
-        client.core.authenticate(self.args[0], self.args[1]).addCallbacks(
+        return client.core.authenticate(self.args[0], self.args[1]).addCallbacks(
             self.on_auth_check,
             self.on_auth_check_err
         )
 
-    def on_connect_err(self, error):
-        reactor.stop()
-
     def on_auth_check(self, result):
         if result:
-            self.result = 0
-        reactor.stop()
+            self.log.info('%s successfully authenticated', self.args[0])
+            return 0
+        else:
+            self.log.info('%s failed to authenticate', self.args[0])
+            return 1
 
     def on_auth_check_err(self, error):
-        reactor.stop()
+        self.log.error('unable to check authentication, vmaild encountered an error')
+        return 1
 
     @argcount(2)
     def run(self):
-        self.result = 1
-        client.connect().addCallbacks(self.on_connect, self.on_connect_err)
-        reactor.run()
-        return self.result
+        return self.connect()
