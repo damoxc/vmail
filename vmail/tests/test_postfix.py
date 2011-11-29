affb16ad4d69ca10e54879e4b74d44906fd6a0e2
Damien Churchill <damien.churchill@ukplc.net>
2011-11-29 17:01:09 +0000

    tests: add postfix test

--- vmail/tests/test_postfix.py
+++ vmail/tests/test_postfix.py
@@ -10,3 +10,6 @@ def test_postfix_queue_stats():
     assert stats['active']   == 0
     assert stats['deferred'] == 78
     assert stats['maildrop'] == 0
+
+def test_postfix_queue_deferred():
+    pass
