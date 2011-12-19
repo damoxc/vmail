a01271e9d9a15b82f251ac4559843ed56251d337
Damien Churchill <damien.churchill@ukplc.net>
2011-12-19 14:03:37 +0000

    health: improve the health checks
    The SMTP checks weren't functioning correctly and also include some
    sanity checks for disk space and system load

--- vmail/daemon/health.py
+++ vmail/daemon/health.py
@@ -23,6 +23,8 @@
 #   Boston, MA    02110-1301, USA.
 #
 
+import os
+
 import poplib
 import socket
 import imaplib
@@ -30,6 +32,22 @@ import smtplib
 
 from vmail.common import get_config
 from vmail.daemon.rpcserver import export
+
+def get_disk_usage(path):
+    disk_data   = os.statvfs(path)
+    block_size  = float(disk_data.f_frsize)
+    free_space  = disk_data.f_bavail * block_size
+    total_space = disk_data.f_blocks * block_size
+    return (free_space / total_space)
+
+def cmp_loadavg(a, b):
+    r = 0
+    for a, b in zip(a, b):
+        if a > b:
+            return 1
+        if a < b:
+            r = -1
+    return r
 
 class Health(object):
 
@@ -39,6 +57,26 @@ class Health(object):
         self.password = config.get('test_password')
 
     @export
+    def check_diskspace(self):
+        try:
+            assert get_disk_usage('/var/spool') > 0.05
+            assert get_disk_usage('/var/log') > 0.05
+        except:
+            return False
+        else:
+            return True
+
+    @export
+    def check_system_load(self):
+        try:
+            loadavg = (float(l) for l in open('/proc/loadavg').read().split())
+            assert cmp_loadavg(loadavg, (5, 5, 5)) != 1
+        except:
+            return False
+        else:
+            return True
+
+    @export
     def check_imap(self):
         imap = None
         try:
@@ -46,6 +84,10 @@ class Health(object):
             imap.login(self.username, self.password)
             imap.select()
             assert imap.state == 'SELECTED'
+        except:
+            return False
+        else:
+            return True
         finally:
             if imap: imap.close()
 
@@ -57,6 +99,10 @@ class Health(object):
             pop.user(self.username)
             pop.pass_(self.password)
             assert pop.noop() == '+OK'
+        except:
+            return False
+        else:
+            return True
         finally:
             if pop: pop.quit()
 
@@ -66,12 +112,18 @@ class Health(object):
         try:
             me = socket.gethostname()
             smtp = smtplib.SMTP(me)
+            code, msg = smtp.helo()
+            assert code == 250, 'SMTP responded with %d %s' % (code, msg)
+
             code, msg = smtp.mail('unknown@example.org')
-            assert code == 250
+            assert code == 250, 'SMTP responded with %d %s' % (code, msg)
 
             code, msg = smtp.rcpt('unknown@example.org')
-            assert code == 250
-
+            assert code == 250, 'SMTP responded with %d %s' % (code, msg)
+        except:
+            return False
+        else:
+            return True
         finally:
             if smtp: smtp.close()
 
@@ -80,10 +132,17 @@ class Health(object):
         smtp = None
         try:
             smtp = smtplib.SMTP('mail6.london.ukplc.net')
+            code, msg = smtp.helo()
+            assert code == 250, 'SMTP responded with %d %s' % (code, msg)
+
             code, msg = smtp.mail('unknown@example.org')
-            assert code == 250
+            assert code == 250, 'SMTP responded with %d %s' % (code, msg)
 
             code, msg = smtp.rcpt('unknown@example.org')
-            assert code == 250
+            assert code == 250, 'SMTP responded with %d %s' % (code, msg)
+        except:
+            return False
+        else:
+            return True
         finally:
             if smtp: smtp.close()
