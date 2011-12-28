57abd6be4bec9da495204379a7e78cd25440cea4
Damien Churchill <damoxc@gmail.com>
2011-12-28 14:13:26 +0000

    tests: fix the postfix tests
    Now we have a selection of queue files in git, make the tests work
    with these files.

--- vmail/tests/test_postfix.py
+++ vmail/tests/test_postfix.py
@@ -7,6 +7,6 @@ def test_postfix_queue_stats():
     postfix.config['postfix_spool'] = os.path.join(os.path.dirname(__file__), 'spool', 'postfix')
     stats = postfix.queue_stats()
 
-    assert stats['active']   == 0
-    assert stats['deferred'] == 78
-    assert stats['maildrop'] == 0
+    assert stats['active']   == 0, 'Actually there are %d' % stats['active']
+    assert stats['deferred'] == 37, 'Actually there are %d' % stats['deferred']
+    assert stats['maildrop'] == 0, 'Actually there are %d' % stats['maildrop']
